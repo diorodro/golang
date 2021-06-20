250f83c81fa903973a29ae5d7183bf7fcd00c6f5
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-20 03:19:16 -0300

    Importando o pacote 'os'

--- hello/hello.go
+++ hello/hello.go
@@ -1,23 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("3 - Sair do Programa")
+	exibeIntroducao()
 
-	var comando int
-	fmt.Scan(&comando)
-
-	if comando == 1 {
-		fmt.Println("Monitoramento...")
+	/*if comando == 1 {
+		fmt.Println("Monitoramento Iniciado...")
 	} else if comando == 2 {
-		fmt.Println("Exibindo Logs")
+		fmt.Println("Exibindo Logs:")
 	} else if comando == 0 {
 		fmt.Println("Saindo...")
 	} else {
 		fmt.Println("Não foi possível completar a operação")
+	}*/
+
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitoramento Iniciado...")
+	case 2:
+		fmt.Println("Exibindo Logs:")
+	case 0:
+		fmt.Println("Saindo...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não foi possível completar a operação")
 	}
+
+}
+
+func exibeIntroducao() {
+	fmt.Println("Seja Bem-Vindo!")
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+
+	return comandoLido
 }
