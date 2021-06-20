3d2b03ee9e8d367c258c2de7897cf35f1414a965
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-20 02:05:26 -0300

    Hello World

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var nome string = "Golang"
-	var idade int
-	var versao float32 = 1.16
+	nome := "Golang"
+	idade := 11
+	versao := 1.16
 	fmt.Println("Este programa está na versão", versao)
 	fmt.Println("O", nome, "foi criado em novembro de 2009, logo tem", idade, "anos")
 }
