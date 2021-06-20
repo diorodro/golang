a42d9fe51d20c43442ff123ba3d38dff7814f3c0
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-20 03:01:37 -0300

    Utilizando operadores condicionais

--- hello/hello.go
+++ hello/hello.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	nome := "Golang"
-	idade := 11
-	versao := 1.16
-	fmt.Println("Este programa está na versão", versao)
-	fmt.Println("O", nome, "foi criado em novembro de 2009, logo tem", idade, "anos")
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("3 - Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	if comando == 1 {
+		fmt.Println("Monitoramento...")
+	} else if comando == 2 {
+		fmt.Println("Exibindo Logs")
+	} else if comando == 0 {
+		fmt.Println("Saindo...")
+	} else {
+		fmt.Println("Não foi possível completar a operação")
+	}
 }
