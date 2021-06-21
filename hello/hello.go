34dc135fa4a415ce98995d7901016444ca05e7f9
Rodrigo Dionizio <rodrigosantiagodionizio@hotmail.com>
2021-06-21 08:18:37 -0300

    Exibindo logs com o pacote 'ioutil' e finalizando projeto

--- hello/hello.go
+++ hello/hello.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,6 +27,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo Logs:")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo...")
 			os.Exit(0)
@@ -74,8 +77,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso : Status", resp.Status)
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site:", site, "está com problemas : Status", resp.Status)
+		registraLog(site, false)
 	}
 }
 
@@ -102,4 +107,28 @@ func leSitesArquivo() []string {
 	arquivo.Close()
 
 	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
